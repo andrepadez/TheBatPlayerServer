08854baba5a364323a779d5642518f2a2c7181fc
Gabe Kangas <gabek@real-ity.com>
2015-01-14 18:32:03 -0800

    Update color test

--- tests/colorTest.js
+++ tests/colorTest.js
@@ -4,7 +4,8 @@ var fs = require('fs');
 var start = new Date();
 
 //var url = "http://userserve-ak.last.fm/serve/500/35850125/Kick+Bong+l_eb03469af44d49daa5dc2d1a4ca5.jpg"; //Dark
-var url = "http://userserve-ak.last.fm/serve/500/80847757/Bruderschaft+5911681563_c082f9b14d_z.jpg"; //Too blue
+//var url = "http://userserve-ak.last.fm/serve/500/80847757/Bruderschaft+5911681563_c082f9b14d_z.jpg"; //Too blue
+var url = "http://userserve-ak.last.fm/serve/_/100954947/Spiral+System.jpg" //dark
 
 var callback = function(colorObject) {
   var html = "<body bgcolor=" + colorObject.hex + "><img src=http://api.thebatplayer.fm/mp3info-dev/artistImage.php?url=" + url + "></body>"
