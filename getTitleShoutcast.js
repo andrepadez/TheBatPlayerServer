ce3db29511713069035ccc0b6d5a4b3b3d23a976
Gabe Kangas <gabek@real-ity.com>
2015-01-17 16:47:04 -0800

    Fix crash when getting no result back.

--- getTitleShoutcast.js
+++ getTitleShoutcast.js
@@ -21,6 +21,7 @@ function getV1Title(url, callback) {
   var res = request(options, function(error, response, body) {
     if (error) {
       console.log(error);
+      callback(null);
     }
     var csv = body.stripTags();
     var csvArray = csv.split(",");
