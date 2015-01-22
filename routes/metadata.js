5c7d7cf48965f91a383f04ab834ef486e7e36441
Gabe Kangas <gabek@real-ity.com>
2015-01-21 20:27:44 -0800

    Add correct cache headers

--- routes/metadata.js
+++ routes/metadata.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var metadata = require("../getMetadata.js");
+var config = require("./config.js");
 
 module.exports = (function() {
   var router = express.Router();
 
   router.get("/:streamurl", function(req, res) {
+    var cacheAge = config.cachetime;
+
+    if (!res.getHeader('Cache-Control')) res.setHeader('Cache-Control', 'public, max-age=' + cacheAge);
+
     req.app.disable('etag');
 
     var url = req.params.streamurl;
