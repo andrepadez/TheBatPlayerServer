ccd0abd8d9b851ec97cb59b7d74c4970124ed95a
Gabe Kangas <gabek@real-ity.com>
2015-01-24 23:51:02 -0800

    More tweaking

--- image/background.js
+++ image/background.js
@@ -16,7 +16,7 @@ function createBackground(url, colorObject, callback) {
 
     utils.download(url, cacheFile, function() {
       var rgb = "'rgb\(" + colorObject.red + "," + colorObject.green + "," + colorObject.blue + "\)'";
-      var command = "convert " + cacheFile + " -colorspace RGB \\( -clone 0 -fill " + rgb + " -colorize 80% -auto-level \\) \\( -clone 0 -colorspace gray \\) -compose blend -define compose:args=80x20 -composite -resize 480x270\^ -gravity center -crop 480x270+0+40 -sigmoidal-contrast 3,50% -colors 5 -normalize -modulate 150,150 -median 5 -sharpen 1x2 -blur 2x2 " + path;
+      var command = "convert " + cacheFile + " -colorspace RGB \\( -clone 0 -fill " + rgb + " -colorize 80% -auto-level \\) \\( -clone 0 -colorspace gray \\) -compose blend -define compose:args=80x20 -composite -resize 480x270\^ -gravity center -crop 480x270+0+40 -sigmoidal-contrast 3,50% -colors 5 -modulate 150,150 -normalize -median 5 -blur 2x2 " + path;
       console.log(command);
 
       var child = exec(command, null, function(err, stdout, stderr) {
