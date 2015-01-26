b844b073102d6b65cd9158f91a5c82aba053de63
Gabe Kangas <gabek@real-ity.com>
2015-01-25 18:02:40 -0800

    Tweaks

--- image/createArtistImage.sh
+++ image/createArtistImage.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # createArtistImage.sh originalImage.jpg "rgb(10,38,80)" outputImage.png
 convert $1 \
+-strip \
 -fill $2 -colorize 20,20,20 \
 \( +clone -alpha extract -virtual-pixel black \
 -spread 30 -blur 0x3 -threshold 50% -spread 10 -blur 10x5 \) \
 -alpha off -compose Copy_Opacity \
--quality 8 \
 -depth 8 \
 -composite png:$3
