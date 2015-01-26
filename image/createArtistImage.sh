ea660afc7ada990215de70dc88d8f7dafc319310
Gabe Kangas <gabek@real-ity.com>
2015-01-25 17:54:53 -0800

    Tweaks

--- image/createArtistImage.sh
+++ image/createArtistImage.sh
@@ -3,6 +3,6 @@
 convert $1 \
 -fill $2 -colorize 20,20,20 \
 \( +clone -alpha extract -virtual-pixel black \
--spread 10 -blur 0x3 -threshold 50% -spread 30 -blur 10x5 \) \
+-spread 30 -blur 0x3 -threshold 50% -spread 10 -blur 10x5 \) \
 -alpha off -compose Copy_Opacity \
 -composite png:$3
