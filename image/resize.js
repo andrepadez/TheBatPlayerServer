90cd06ff93cc272dcfa7b4bd5fc40f4a76f991f7
Gabe Kangas <gabek@real-ity.com>
2015-01-25 21:16:40 -0800

    Resizing

--- image/resize.js
+++ image/resize.js
@@ -15,7 +15,7 @@ function resizeImage(url, width, height, callback) {
     }
 
     utils.download(url, cacheFile, function() {
-      var size = width + "X" + height + ">";
+      var size = "\"" + width + "X" + height + ">\"";
       var command = "convert " + cacheFile + " -resize " + size + " -gravity SouthEast -append ./image/resources/smallbat.png -strip -quality 95 -composite " + path;
       console.log(command);
 
