9209377f65d680dfd9742a0b68d7fe7c09942171
Gabe Kangas <gabek@real-ity.com>
2015-01-25 17:45:33 -0800

    Tweaks

--- image/createArtistImage.sh
+++ image/createArtistImage.sh
@@ -4,6 +4,6 @@ convert $1 \
 -fill $2 -colorize 0,0,80 \
 -alpha set -virtual-pixel transparent -channel A -blur 0x8  -level 30%,100% +channel \
 \( +clone -alpha extract -virtual-pixel black \
--spread 10 -blur 0x3 -threshold 50% -spread 4 -blur 10x.7 \) \
+-spread 10 -blur 0x3 -threshold 50% -spread 40 -blur 10x5 \) \
 -alpha off -compose Copy_Opacity \
 -composite png:$3
