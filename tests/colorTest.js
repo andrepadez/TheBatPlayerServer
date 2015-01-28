1a0df4d7c28a48887e501002028bece956e007cc
Gabe Kangas <gabek@real-ity.com>
2015-01-27 18:46:47 -0800

    BG tweak

--- tests/colorTest.js
+++ tests/colorTest.js
@@ -9,8 +9,8 @@ var start = new Date();
 //var url = "http://userserve-ak.last.fm/serve/500/35850125/Kick+Bong+l_eb03469af44d49daa5dc2d1a4ca5.jpg"; //Dark
 //var url = "http://userserve-ak.last.fm/serve/500/80847757/Bruderschaft+5911681563_c082f9b14d_z.jpg"; //Too blue
 //var url = "http://userserve-ak.last.fm/serve/_/100954947/Spiral+System.jpg" //dark
-var url = "http://userserve-ak.last.fm/serve/500/197829/Lords+of+Acid+Violet.jpg"; // Dark purple
-//var url = "http://userserve-ak.last.fm/serve/_/433759/Frozen+Plasma.jpg"; //Should return blue but returns pink
+//var url = "http://userserve-ak.last.fm/serve/500/197829/Lords+of+Acid+Violet.jpg"; // Dark purple
+var url = "http://userserve-ak.last.fm/serve/_/433759/Frozen+Plasma.jpg"; //Should return blue but returns pink
 //var url = "http://userserve-ak.last.fm/serve/500/2535509/Nine+Inch+Nails+nin.jpg"; //Red but returns yellow
 
 var callback = function(colorObject) {
