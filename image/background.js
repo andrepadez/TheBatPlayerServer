6632489cb0f888c33ce353204158181dc5e68464
Gabe Kangas <gabek@real-ity.com>
2015-01-30 18:39:51 -0800

    bg teaks

--- image/background.js
+++ image/background.js
@@ -19,7 +19,7 @@ function createBackground(url, colorObject, callback) {
     utils.download(url, cacheFile, function() {
 
       var rgb = "'rgb\(" + colorObject.red + "," + colorObject.green + "," + colorObject.blue + "\)'";
-      var command = "convert " + cacheFile + " -colorspace gray -colorspace RGB -resize 480x270\^ -morphology Open Octagon -gravity center -crop 480x270+0+40 -median 8 -fill " + rgb + " -colorize 18% -auto-level -auto-gamma -brightness-contrast -20x27 " + path;
+      var command = "convert " + cacheFile + " -colorspace gray -colorspace RGB -resize 480x270\^ -morphology Open Octagon -gravity center -crop 480x270+0+40 -median 8 -fill " + rgb + " -colorize 20% -auto-level -auto-gamma -brightness-contrast -20x27 " + path;
       console.log(command);
 
       var childCallback = function(err, stdout, stderr) {
