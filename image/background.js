a68255ed9bc8b8e786b6e5e6ae86e4c7c2d6ec06
Gabe Kangas <gabek@real-ity.com>
2015-02-14 21:46:51 -0800

    More bg tweaks

--- image/background.js
+++ image/background.js
@@ -18,7 +18,7 @@ function createBackground(url, colorObject, callback) {
     utils.download(url, cacheFile, function() {
 
       var rgb = "'rgb\(" + colorObject.red + "," + colorObject.green + "," + colorObject.blue + "\)'";
-      var command = "convert " + cacheFile + " -depth 8 -colorspace gray -colorspace RGB  -resize 480x270\^ -morphology Open Octagon -gravity North -crop 480x270+0+0\! +repage -median 6 -fill " + rgb + " -auto-level -auto-gamma -colorize 35% -brightness-contrast -22x27 -sigmoidal-contrast 16x20% -strip " + path;
+      var command = "convert " + cacheFile + " -depth 8 -colorspace gray -colorspace RGB  -resize 480x270\^ -morphology Open Octagon -gravity North -crop 480x270+0+50 -median 6 -fill " + rgb + " -auto-level -auto-gamma -colorize 35% -brightness-contrast -22x27 -sigmoidal-contrast 16x20% -strip " + path;
 
       var childCallback = function(err, stdout, stderr) {
         if (!err && !stderr) {
