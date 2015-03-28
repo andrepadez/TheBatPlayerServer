5ddd9daceaac11518d35e4d117d6fc1a4817a39e
Gabe Kangas <gabek@real-ity.com>
2015-03-28 14:59:15 -0700

    Don't add a cache header since it is no longer being used.

--- routes/metadata.js
+++ routes/metadata.js
@@ -14,11 +14,12 @@ module.exports = (function() {
         return next(error);
       }
 
-      var cacheAge = config.cachetime;
-      res.setHeader('Cache-Control', 'public, max-age=' + cacheAge);
-      res.setHeader("Content-Type", "application/json; charset=utf-8");
+      // Since I'm not putting this through a CDN anymore don't set this.
+      // var cacheAge = config.cachetime;
+      // res.setHeader('Cache-Control', 'public, max-age=' + cacheAge);
+      // res.setHeader("Content-Type", "application/json; charset=utf-8");
 
-      res.json(result);
+      return res.json(result);
     });
   });
 
